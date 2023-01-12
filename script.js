1a4181b6e74f1d722079fdb4a2d7755dd6443652
Heon Soo Park <hspark220@gmail.com>
2023-01-12 12:51:33 -0500

    Changed markboard logic
    also added checkBoard

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const gameBoard = (() => {
     ]
 
     const markBoard = (position, mark) => {
-        board[position] = mark;
+        _board[position] = mark;
+        console.log(_board);
 
     }
 
@@ -14,25 +15,75 @@ const gameBoard = (() => {
         return _board[position];
     }
 
-    return {markBoard, getBoardMark}
+    const checkBoard = () => {
+        if ((_board[0] === _board[1] && _board[1] === _board[2] && _board[0] != "") ||
+            (_board[0] === _board[4] && _board[4] === _board[8] && _board[0] != "") ||
+            (_board[0] === _board[3] && _board[3] === _board[6] && _board[0] != "") ||
+            (_board[2] === _board[5] && _board[5] === _board[8] && _board[2] != "") ||
+            (_board[2] === _board[4] && _board[4] === _board[6] && _board[2] != "") ||
+            (_board[6] === _board[7] && _board[8] === _board[7] && _board[6] != "") ||
+            (_board[1] === _board[4] && _board[4] === _board[7] && _board[1] != "") ||
+            (_board[3] === _board[4] && _board[4] === _board[5] && _board[3] != "")
+            ){
+                console.log("GAME ENDED");
+            }
+        else {
+            if (_board.includes('') === true) {
+                return;
+            } else {
+                console.log("IT'S A DRAW");
+            }
+        }
+    }    
+
+    return {markBoard, getBoardMark, checkBoard}
 })();
 
 const player = (_name, _mark) => {
-    const markBox = (position) => {
-        gameBoard.markBoard(position, _mark);
+    const getName = () => {
+        return _name;
     }
 
-    return {markBox}
+    const getMark = () => {
+        return _mark;
+    }
+
+    return {getMark}
 }
 
-const gameDisplay = (() => {
+const game = ((player1, player2) => {
+    let _playerTurn = player1;
+
     const printBoard = () => {
         for(let i = 0; i < 9; i++) {
             const box = document.getElementById(`box${i+1}`);
-            box.append(gameBoard.getBoardMark(i));
+            box.addEventListener('click', _markBox);
         }
     };
-    return {printBoard};
+
+    const _markBox = (e) => {
+        const position = e.target.getAttribute('id')[3]-1;
+        if(gameBoard.getBoardMark(position) === ''){
+            //change to player's marking
+            e.target.append('X');
+            gameBoard.markBoard(position, 'X');
+            gameBoard.checkBoard();
+        } else {
+            console.log("SOME ERROR MESSAGE?")
+        }
+    }
+
+    const playGame = () => {
+        player1 = new player('xania', 'X');
+        player2 = new player('apeach', 'O');
+
+    }
+
+    
+    
+    
+    return{printBoard}
+
 })();
 
-gameDisplay.printBoard();
+game.printBoard();
